aa43e51c871efc9401b29f4b7fbe7a6c83baec95
Joao Francisco Cocca Fukuda <joao.fukuda@protonmail.com>
2020-05-17 02:04:17 -0300

    feat(core): Remade in linux socket library
    Remove the Asio library (it was giving some errors and I don't know how
    to use Asio, so I changed to linux's socket programming library.
    Add option to set port (default is 27090, can change on 3rd argument).
    
    Close issue: #1

--- src/main.cpp
+++ src/main.cpp
@@ -3,51 +3,73 @@
 #include <string>
 #include <sstream>
 
-#include <asio.hpp>
-
-using asio::ip::tcp;
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
-	if(argc != 2) return 0;
+	int port = 27090;
+	if(argc == 1) return 0;
+	if(argc == 3)
+	{
+		std::stringstream toint(argv[2]);
+		toint >> port;
+	}
 
 	std::string filename(argv[1]);
 
-	std::ifstream file(filename, std::ios::ate);
+	std::ifstream file(filename);
 	if(!file) return 0;
 
-	auto filesize = file.tellg();
-	file.seekg(0);
-
-	std::cout << "Sharing " << filename << " (" << filesize << " bytes)..." << std::endl;
-
-	std::string filebuffer;
-
-	char buf[512];
-	while(file.read(buf, 512).gcount() > 0) filebuffer.append(buf);
+	std::string filebuffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	filebuffer += "\r\n\0";
 
 	file.close();
 
-	try
+	std::cout << "Sharing " << filename << " (" << (filebuffer.length() - 3) << " bytes) on localhost:" << port << "..." << std::endl;
+
+	auto host_sock = socket(AF_INET, SOCK_STREAM, 0);
+
 	{
-		asio::io_context io_context;
-		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 27090));
-		tcp::socket socket(io_context);
-		acceptor.accept(socket);
-		asio::error_code ignored_error;
+		int opt(1);
+		setsockopt(host_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+	}
+
+	sockaddr_in host_info;
+	host_info.sin_family = AF_INET;
+	host_info.sin_port = htons(port);
+	host_info.sin_addr.s_addr = INADDR_ANY;
+
+	bind(host_sock, reinterpret_cast<sockaddr*>(&host_info), sizeof(host_info));
+
+	listen(host_sock, 3);
+
+	while(true)
+	{
+		sockaddr_in client_info;
+		auto client_size = sizeof(client_info);
+		auto client_sock = accept(host_sock, reinterpret_cast<sockaddr*>(&client_info), reinterpret_cast<socklen_t*>(&client_size));
+
+		std::cout << "Sending file" << std::endl;
 
 		{
 			std::stringstream buf;
 			buf << "HTTP/1.1 200 OK\r\n"
 				<< "Server: HTTPer\r\n"
-				<< "Content-Length: " << filesize << "\r\n"
+				<< "Content-Length: " << ( filebuffer.size() - 3 ) << "\r\n"
 				<< "Content-Disposition: attachment; filename=\"" << filename << "\"\r\n"
-				<< "Content-Type: text/plain\r\n\r\n";
-			asio::write(socket, asio::buffer(buf.str()), ignored_error);
+				<< "Content-Type: text/plain\r\n\r\n\0";
+			send(client_sock, buf.str().c_str(), buf.str().size(), 0);
 		}
-		asio::write(socket, asio::buffer(filebuffer), ignored_error);
-	} catch(std::exception e)
-	{
-		std::cerr << e.what() << std::endl;
+
+		send(client_sock, filebuffer.c_str(), filebuffer.size() - 2, 0);
+
+		close(client_sock);
+
+		std::cout << "File sent" << std::endl;
 	}
+
+	close(host_sock);
 }
+
