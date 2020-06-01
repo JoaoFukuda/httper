53e3b9b82d0867f181f770770e7ba03052ecf6c5
João Francisco Cocca Fukuda <joao.fukuda@protonmail.com>
2020-06-01 14:38:35 -0300

    refactor(core): Change to default coding style

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,24 @@ int main(int argc, char** argv)
 	std::string filename(argv[1]);
 	std::ifstream file(filename);
 	[[unlikely]] if (!file) {
-		std::cerr << "Error: No file " << filename << " found." << std::endl;
+		std::cerr << "Error: No file " << filename << " found" << std::endl;
 		return 2;
 	}
 
 	// Loads the file into memory by putting it in this string
-	std::string filebuffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	std::string filebuffer((std::istreambuf_iterator<char>(file)),
+			std::istreambuf_iterator<char>());
 	filebuffer += "\r\n\0";
 
 	file.close();
 
-	std::cout << "Sharing " << filename << " (" << (filebuffer.length() - 3) << " bytes) on localhost:" << port << "..." << std::endl;
+	std::cout << "Sharing " << filename
+			<< " (" << (filebuffer.length() - 3) << " bytes) on localhost:" << port
+			<< "..." << std::endl;
 
 	auto host_sock = socket(AF_INET, SOCK_STREAM, 0);
 	[[unlikely]] if (host_sock == -1) {
-		std::cerr << "Error: Socket could not be created." << std::endl;
+		std::cerr << "Error: Socket could not be created" << std::endl;
 		return 3;
 	}
 
@@ -44,7 +47,11 @@ int main(int argc, char** argv)
 		// Sets socket options for the system not to block the ports for
 		// subsequent uses
 		int opt(1);
-		setsockopt(host_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+		setsockopt(host_sock,
+				SOL_SOCKET,
+				SO_REUSEADDR | SO_REUSEPORT,
+				&opt,
+				sizeof(opt));
 	}
 
 	sockaddr_in host_info;
@@ -53,17 +60,16 @@ int main(int argc, char** argv)
 	host_info.sin_addr.s_addr = INADDR_ANY;
 
 	if (bind(host_sock, reinterpret_cast<sockaddr*>(&host_info), sizeof(host_info)) == -1) {
-		std::cerr << "Error: Could not bind socket to port " << port << '.' << std::endl;
+		std::cerr << "Error: Could not bind socket to port " << port << std::endl;
 		return 4;
 	}
 
 	[[unlikely]] if (listen(host_sock, 3) == -1) {
-		std::cerr << "Error: Could not pot socket to listen." << std::endl;
+		std::cerr << "Error: Could not pot socket to listen" << std::endl;
 		return 5;
 	}
 
-	while(true)
-	{
+	while (true) {
 		sockaddr_in client_info;
 		auto client_size = sizeof(client_info);
 
@@ -72,7 +78,7 @@ int main(int argc, char** argv)
 				reinterpret_cast<socklen_t*>(&client_size));
 
 		[[unlikely]] if (client_sock == -1) {
-			std::cerr << "Error: Client could not be accepted." << std::endl;
+			std::cerr << "Error: Client could not be accepted" << std::endl;
 			continue;
 		}
 
